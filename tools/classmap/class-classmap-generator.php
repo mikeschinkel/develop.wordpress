162278d55fbc70bb1eb7f093325e111f1d56b628
Mike Schinkel <mikeschinkel@gmail.com>
2016-09-24 21:38:50 -0400

    Added functionality to omit classes from the classmap.

--- tools/classmap/class-classmap-generator.php
+++ tools/classmap/class-classmap-generator.php
@@ -41,6 +41,19 @@ class WP_Classmap_Generator {
 	}
 
 	/**
+	 * @param string[] $class_names
+	 */
+	public function omit_classes( $class_names ) {
+
+		foreach ( $class_names as $class_name ) {
+
+			unset( $this->_classmap[ $class_name ] );
+
+		}
+
+	}
+
+	/**
 	 * @param SplFileInfo $file
 	 * @return string|null
 	 */
@@ -74,24 +87,20 @@ class WP_Classmap_Generator {
 	 */
 	private function _get_file_classnames( $file ) {
 
-		do {
+		$classes = array();
 
-			$classes = array();
+		$php_code = file_get_contents( $file->getRealPath() );
 
-			$php_code = file_get_contents( $file->getRealPath() );
+		/**
+		 * @see http://stackoverflow.com/a/12011255/102699
+		 */
+		$token = '([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)';
 
-			/**
-			 * @see http://stackoverflow.com/a/12011255/102699
-			 */
-			$token = '([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)';
-
-			if ( ! preg_match_all( "/\n\s*(final|abstract)?\s*(class|interface|trait)\s+{$token}/", $php_code, $matches ) ) {
-				break;
-			}
+		if ( preg_match_all( "/\n\s*(final|abstract)?\s*(class|interface|trait)\s+{$token}/", $php_code, $matches ) ) {
 
 			$classes = $matches[ 3 ];
 
-		} while ( false );
+		}
 
 		return $classes;
 
--- wp-classmap-generator.php
+++ wp-classmap-generator.php
@@ -5,5 +5,19 @@ require __DIR__ . '/tools/classmap/class-classmap-generator.php';
 $generator = new WP_Classmap_Generator( __DIR__ . '/src' );
 $generator->add_files( 'wp-admin' );
 $generator->add_files( 'wp-includes' );
+
+/**
+ * The following shows how to omit classes that we want to laod manually
+ * This is not required by instead added because of these trac comments:
+ * @link https://core.trac.wordpress.org/ticket/36335?replyto=218#comment:218
+ */
+$generator->omit_classes( array(
+	'WP',
+	'WP_Query',
+	'WP_Post',
+	'WP_Rewrite',
+	'wpdb',
+));
+
 $classmap = $generator->get_classmap();
 file_put_contents( __DIR__ . '/src/wp-classmap.php', $classmap );
